1424e7acc17d287b2548dbb59c00b90e904942a2
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-13 07:22:37 -0700

    added schema for project

---
+++ src/util/result-set-mapper.ts
@@ -0,0 +1,36 @@
+import {UserSchema} from './schemas';
+import {ReimbursementsSchema} from './schemas';
+import {Users} from '../models/users';
+import {Reimbursements} from '../models/reimbursements';
+
+export function mapUserResultSet(resultSet: UserSchema){
+	if(!resultSet){
+		return {} as Users;
+	}
+	return new Users(
+		resultSet.ers_user_id,
+		resultSet.username,
+		resultSet.password,
+		resultSet.first_name,
+		resultSet.last_name,
+		resultSet.email,
+		resultSet.role_name
+	)
+}
+
+export function mapReimbursementsSet(resultSet: ReimbursementsSchema){
+	if(!resultSet){
+		return {} as Reimbursements;
+	}
+	return new Reimbursements(
+		resultSet.reimb_id,
+		resultSet.amount,
+		resultSet.submitted,
+		resultSet.resolved,
+		resultSet.description,
+		resultSet.author_id,
+		resultSet.resolver_id,
+		resultSet.reimb_status_id,
+		resultSet.reimb_type_id
+	)
+}
---
+++ src/util/schemas.ts
@@ -0,0 +1,21 @@
+export interface UserSchema{
+	ers_user_id: number;
+	username: string;
+	password: string;
+	first_name: string;
+	last_name: string;
+	email: string;
+	role_name: string;
+}
+
+export interface ReimbursementsSchema{
+	reimb_id: number;
+	amount: number;
+	submitted: Date;
+	resolved: Date;
+	description: string;
+	author_id: number;
+	resolver_id: number;
+	reimb_status_id: number;
+	reimb_type_id: number;
+}
