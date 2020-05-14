cad9f50561ee57174512991170a5833675425ba5
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-13 18:52:07 -0700

    added service for getReimById, getReimbByUniqueKey, getAllReimbByType, getAllReimbByStatus, isValidType, isValidStatus. All still need to be test so changes will be made

--- src/services/reimb-service.ts
+++ src/services/reimb-service.ts
@@ -18,4 +18,90 @@ export class ReimbService{
 		}
 		return reimb;
 	}
+
+	async getReimbById(id:number): Promise<Reimbursements>{
+		if(!isValidId(id)){
+			throw new BadRequestError();
+		}
+		let reimb = {...await this.reimbRepo.getById(id)};
+		if(isEmptyObject(reimb)){
+			throw new ResourceNotFoundError()
+		}
+		return reimb;
+	}
+	async getReimbByUniqueKey(queryObj: any): Promise<Reimbursements>{
+		
+		let queryKeys = Object.keys(queryObj);
+		
+		if(!queryKeys.every(key => isPropertyOf(key, Reimbursements))){
+			throw new BadRequestError('Invalid key provided please provided a valid key');
+		}
+		let key = queryKeys[0];
+		let val = queryObj[key];
+		if(key === 'reimb_id'){
+			return await this.getReimbById(+val);
+		}
+		if(!isValidStrings(val)){
+			throw new BadRequestError();
+		}
+		let reimb = await this.reimbRepo.getReimbByUniqueKey(key, val);
+		if(isEmptyObject(reimb)){
+			throw new ResourceNotFoundError();
+		}
+		return reimb;
+	}
+
+	async getAllReimbByType(type:string): Promise<Reimbursements[]>{
+		if(!isValidStrings(type)){
+			throw new BadRequestError();
+		}
+		let reimbType = await this.reimbRepo.getAllByType(type);
+		if(!reimbType){
+			throw new BadRequestError("the type of Reimbursement is invalid")
+		}
+
+		let reimb = await this.reimbRepo.getAllByType(type);
+		if(reimb.length === 0){
+			throw new ResourceNotFoundError();
+		}
+		return reimb;
+	}
+
+	async getAllReimbByStatus(status: string): Promise<Reimbursements[]>{
+		if(!isValidStrings(status)){
+			throw new BadRequestError();
+		}
+		let reimbStatus = await this.reimbRepo.getAllByStatus(status);
+		if(!reimbStatus){
+			throw new BadRequestError('the status of Reimbursement given is invalid');
+		}
+
+		let reimb = await this.reimbRepo.getAllByStatus(status);
+		if(reimb.length === 0){
+			throw new ResourceNotFoundError();
+		}
+		return reimb;
+	}
+
+	async isValidType(type:string): Promise<boolean>{
+		try{
+			await this.getReimbByUniqueKey({'reimb_status': type});
+		}catch(e){
+			console.log('type is not in database');
+			return false;
+		}
+		console.log('type is in database');
+		return true;
+	}
+
+	async isValidStatus(status: string): Promise<boolean>{
+		try{
+			await this.getReimbByUniqueKey({'reimb_status': status});
+		}catch(e){
+			console.log('status is not in database');
+			return false;
+		}
+		console.log('status is in database');
+		return true;
+	}
 }
