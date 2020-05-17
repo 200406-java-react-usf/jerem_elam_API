813fa1cd7e05a77884ce136509f4f5500a2207fc
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-16 21:03:59 -0700

    changed principal name to match with the database

--- src/dtos/principal.ts
+++ src/dtos/principal.ts
@@ -2,11 +2,11 @@ export class Principal {
 
     id: number;
     username: string;
-    role: string;
+    role_name: string;
 
-    constructor(id: number, un: string, role: string) {
+    constructor(id: number, un: string, role_name: string) {
         this.id = id;
         this.username = un;
-        this.role = role;
+        this.role_name = role_name;
     }
 }
