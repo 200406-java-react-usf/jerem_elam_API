d906406e1f5d298ae80ce339ae15e557f19118a0
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-18 20:10:49 -0700

    switch cor-filter to localhost

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -1,8 +1,8 @@
 import{Request, Response} from 'express';
 
 export function corsFilter(req: Request, resp: Response, next){
-	// resp.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-	resp.header('Access-Control-Allow-Origin', 'http://jeremy-java-react-revature.s3-website.us-east-2.amazonaws.com');
+	resp.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	// resp.header('Access-Control-Allow-Origin', 'http://jeremy-java-react-revature.s3-website.us-east-2.amazonaws.com');
 	resp.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
 	resp.header('Access-Control-Allow-Credentials','true');
 	resp.header('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
